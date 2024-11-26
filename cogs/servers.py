04af2dd962ca7641ff6442ec5b3adb1fd6208943
Bi0force1 <Bi0force1@hotmail.com>
2024-11-25 19:49:19 -0600

    commit this

--- cogs/servers.py
+++ cogs/servers.py
@@ -3,15 +3,11 @@ import discord
 from discord.ext import commands
 import psutil
 
-
-
 processes = {
     "ark": "ArkAscendedServer.exe",
     "palworld": "PalServer-Win64-Shipping-Cmd.exe",
     "enshrouded": "enshrouded.exe",
-    # Add more processes as needed
-}
-
+    }
 
 class Hosting(commands.Cog):
     def __init__(self, client):
@@ -19,11 +15,9 @@ class Hosting(commands.Cog):
 
     @commands.command()
     async def server(self, ctx):
-        # Define a dictionary of processes to check
 
         running_processes = []
 
-        # Check each process to see if it is running
         for name, process_name in processes.items():
             if any(proc.name() == process_name for proc in psutil.process_iter()):
                 running_processes.append(f"**{name}**")
@@ -32,7 +26,6 @@ class Hosting(commands.Cog):
             await ctx.send(f"The following server(s) are running:   {', '.join(running_processes)}.")
         else:
             await ctx.send("No specified servers are currently running.")
-
 
 async def setup(client):
     await client.add_cog(Hosting(client))
